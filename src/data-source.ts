f828d4a344707f4df5c7d2116f72966b7919b6c6
ayush <ayushkhatri.105@gamil.com>
2024-04-05 10:16:42 +0545

    commit

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,14 +9,15 @@ import { library } from "./entity/library"
 import { vehicle } from "./entity/vehicle"
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "9818430363ayush",
-    database: "traning",
-    // url:"postgres://default:DAYd8igv9IMu@ep-icy-heart-a4ljfrot-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
-    synchronize: true,
+    // type: "mysql",
+    type:"postgres",
+    // host: "localhost",
+    // port: 3306,
+    // username: "root",
+    // password: "9818430363ayush",
+    // database: "traning",
+    url:"postgres://default:DAYd8igv9IMu@ep-icy-heart-a4ljfrot-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    synchronize: false,
     logging: false,
     entities: [User, employe, Student,teacher,newuser,library,vehicle],
     migrations: [],
